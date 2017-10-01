0353e9109d2e2a318f287db37b8c44e0f2abc1c8
fcracker79 <fcracker79@gmail.com>
2017-10-01 13:45:16 +0200

    First chapter.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+extern crate rand;
+use std::io;
+use std::io::Write;
+use rand::Rng;
+use std::cmp::Ordering;
+
+
 fn main() {
-    println!("Hello, world!");
+    main_guess_number()
+}
+
+fn main_guess_number() {
+    println!("Guess number!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    loop {
+        print!("Please insert your number ({}): ", secret_number);
+        io::stdout().flush().expect("Failed to flush standard output");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(x) => {
+                println!("Dino {}", x);
+                continue
+            }
+        };
+        println!("You guessed {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break
+            },
+        }
+    }
 }
